97e6c4fa61f420828ab139e11329bd22a8b3c915
your_username <henrythorogood@icloud.com>
2025-10-19 22:45:27 +0100

    Added Lexus and fixed errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     "./src/**/*.{ts,tsx}",
     "*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix: "",
   theme: {
     container: {
       center: true,
@@ -51,8 +52,6 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
-        "gjc-yellow": "hsl(var(--gjc-yellow))",
-        "gjc-yellow-hover": "hsl(var(--gjc-yellow-hover))",
       },
       borderRadius: {
         lg: "var(--radius)",
@@ -61,12 +60,12 @@ module.exports = {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
+          from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+          to: { height: "0" },
         },
       },
       animation: {
